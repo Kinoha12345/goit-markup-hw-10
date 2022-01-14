e2b903e1f0d8d7f78782a5a0c78d488c122563aa
Kinoha12345 <filin.vlad.v@gmail.com>
2022-01-14 19:51:11 +0200

    old

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,12 @@ const contris = 'https://restcountries.com/v3.1/name/';
 const params = '?fields=name,capital,population,flags,languages';
 const fetchCountries = function fetchCountries(url) {
     console.log(url);
-    return fetch(contris + url + params).then(response => response.json());
-};
+    return fetch(contris + url + params).then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+    }
     
 export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -9,37 +10,55 @@ const refs = {
     ul: document.querySelector('.country-list'),
     div: document.querySelector('.country-info')
 }
+let inputValue = '';
 
 refs.input.addEventListener('input', debounce(getInput, DEBOUNCE_DELAY))
 
+function contryList (data){
+  const markup = data
+  .map(
+    item => `<li>
+    <div class ="block"><img src="${item.flags.svg}" alt="">
+    <p class ="text-name"><b> ${item.name.common}</b></p></div>
+</li>`).join('');
+return markup;
+}
+
+function dataItem (data){
+  const dataItem = data
+  .map(
+    item => `<div class ="block"><img src="${item.flags.svg}" alt="">
+    <p class ="text-name"><b> ${item.name.common}</b></p></div>
+    <p><b>Capital:</b> ${item.capital}</p>
+    <p><b>Population:</b> ${item.population}</p>
+    <p><b>Languages:</b> ${Object.values(item.languages)}</p>`).join('');
+    return dataItem;
+}
 
 function getInput() {
-    const inputValue = refs.input.value;
+  inputValue = refs.input.value.trim();
+  if (inputValue === '') {
+    refs.ul.innerHTML ='';
+    refs.div.innerHTML = '';
+    return};
     fetchCountries(inputValue).then(data => {
-      const markup = data
-        .map(item => `<li>
-     <img src="${item.flags.svg}">
-   </li>`,
-        )
-        .join('');
-      const dataItem = data
-        .map(
-          item => `<li>
-     <img src="${item.flags.svg}">
-   </li>`
-        )
-        .join('');
-  console.log(inputValue);
-      if (data.length > 1) {
-        refs.ul.insertAdjacentHTML('afterbegin', markup);
+   
+      if (data.length >= 10) {
+        refs.ul.innerHTML = '';
+        refs.div.innerHTML = '';
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        return
       }
-      if (data.length > 10) {
-        alert('Too many matches found. Please enter a more specific name.');
-      }
-      // if (data.length === 1) {
-      //   refs.div.innerHTML();
-      // }
-    });
-  }
-
-
+    if (data.length > 1) {
+      refs.ul.innerHTML ='';
+      refs.div.innerHTML = '';
+      refs.ul.insertAdjacentHTML('afterbegin', contryList(data));
+      return;
+    }
+    refs.ul.innerHTML = '';
+    
+    refs.div.innerHTML = dataItem(data);
+  }).catch(()=>{
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+  })
+}
